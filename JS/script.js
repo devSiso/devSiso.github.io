f6aff2b27e6c9cf6356f510158a667e7c683eae9
devSiso <flxvingat@gmail.com>
2017-11-27 15:24:02 -0200

    Adicionei alguns estilos, terminei o texto 'sobre mim' e adicionei o botao de voltar ao topo, com animacao

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('#topBtn').fadeIn();
-        } else {
-            $('#topBtn').fadeOut();
-        }
-    });
-
     $('#topBtn').click(function(){
-        $('html, body').animate({scrollTop : 0},800);
-        return false;
+        $('html, body').animate({scrollTop:0}, 'slow');
     });
 });
