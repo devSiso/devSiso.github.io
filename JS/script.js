07b0d6a80fd1410a5be4869bd7fd19870e603e3f
devSiso <flxvingat@gmail.com>
2017-11-27 16:30:56 -0200

    Ajustei o botao de subir para o topo, adicionei um hover no botao, e comecei o footer

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
     $('#topBtn').click(function(){
-        $('html, body').animate({scrollTop:0}, 'slow');
+        $('html, body').animate({scrollTop:0},500);
     });
+
+    $('#myHistoryScroll').click(function(){
+        $('html, body').animate({scrollTop:$('#scrollHistoryTitle').offset().top }, 1000);
+    });
+    var posicaoInicial = $('#aboutMe').position().top;
+
+    $(window).scroll(function () { // oscultador de scroll
+    var posicaoScroll = $(window).scrollTop(); // obtem a quantidade de scroll no momento
+    console.log(posicaoScroll);
+     if (posicaoScroll > 540){
+         $('#topBtn').show(); //856
+     } 
+     if (posicaoScroll == 0) {
+        $('#topBtn').hide();
+     }
 });
+});
+
