6e3a38d4fcc308fbe550fd513e2543a26d367cd9
devSiso <flxvingat@gmail.com>
2017-11-29 19:16:11 -0200

    Troquei os icons da aba Meus Contatos

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-    $('#topBtn').click(function(){
-        $('html, body').animate({scrollTop:0},500);
+$(document).ready(function () {
+    $('#topBtn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
     });
 
-    $('#myHistoryScroll').click(function(){
-        $('html, body').animate({scrollTop:$('#scrollHistoryTitle').offset().top }, 1000);
+    $('#myHistoryScroll').click(function () {
+        $('html, body').animate({ scrollTop: $('#scrollHistoryTitle').offset().top }, 1000);
     });
-     $('#contacts').click(function(){
-        $('html, body').animate({scrollTop:$('#h1Contacts').offset().top }, 1200);
-        $('#number').show(1600);   
+    $('#contacts').click(function () {
+        $('html, body').animate({ scrollTop: $('#h1Contacts').offset().top }, 1200);
+        $('#number').show(1600);
     });
     var posicaoInicial = $('#aboutMe').position().top;
 
     $(window).scroll(function () { // oscultador de scroll
-    var posicaoScroll = $(window).scrollTop(); // obtem a quantidade de scroll no momento
-    console.log(posicaoScroll);
-     if (posicaoScroll > 540){
-         $('#topBtn').show(); //856
-     } 
-     if (posicaoScroll == 0) {
-        $('#topBtn').hide();
-     }
-});
+        var posicaoScroll = $(window).scrollTop(); // obtem a quantidade de scroll no momento
+        console.log(posicaoScroll);
+        if (posicaoScroll > 540) {
+            $('#topBtn').show(); //856
+        }
+        if (posicaoScroll == 0) {
+            $('#topBtn').hide();
+        }
+    });
 });
 
