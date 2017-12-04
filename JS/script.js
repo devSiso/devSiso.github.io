0f471138815dc2bf7150ca1ae7cfed1d4163bf0f
devSiso <flxvingat@gmail.com>
2017-12-04 16:30:23 -0200

    Removi as setas do carousel, adicionei a aba moments com estilizacao melhorei a interacao com os cards e com a navbar

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
     $('#topBtn').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 500);
+        $('html, body').animate({ scrollTop: 0 }, 700);
     });
 
     $('#myHistoryScroll').click(function () {
-        $('html, body').animate({ scrollTop: $('#scrollAboutMe').offset().top }, 1000);
+        $('html, body').animate({ scrollTop: $('#scrollAboutMe').offset().top - 60 }, 1000);
     });
     $('#contacts').click(function () {
         $('html, body').animate({ scrollTop: $('#h1Contacts').offset().top }, 1200);
     });
+    $('#scrollMoments').click(function () {
+        $('html, body').animate({ scrollTop: $('#momentAncora').offset().top - 115 }, 1200);
+    });
     $('#myExperienceScroll').click(function () {
-        $('html, body').animate({ scrollTop: $('#scrollmyExperience').offset().top }, 1000);
+        $('html, body').animate({ scrollTop: $('#scrollmyExperience').offset().top - 100 }, 1000);
     });
     var posicaoInicial = $('#aboutMe').position().top;
 
